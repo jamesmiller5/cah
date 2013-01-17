3a95a1387a198593efe94e21694e068a26ec00df
James Miller V <mille168@purdue.edu>
2013-01-17 14:50:58 -0500

    Bare bones andriod client with simple build scripts
    Test=Na
    Task=Setup build environment for android client

---
+++ android-client/src/com/cah/Cah.java
@@ -0,0 +1,15 @@
+package com.cah;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Cah extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
