65dc643378fb5726614b4dc56b6270647bc38648
Christina Atallah <aphex.tinez@gmail.com>
2013-02-02 23:40:08 -0500

    Add crown and portraits to table view

--- android-client/src/com/cah/Cah.java
+++ android-client/src/com/cah/Cah.java
@@ -1,6 +1,8 @@
 package com.cah;
 
 import android.app.Activity;
+import android.widget.ImageView;
+import android.widget.QuickContactBadge;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -15,18 +17,22 @@ public class Cah extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.game);
-        
-        Button button = (Button) this.findViewById(R.id.testButton);
-        button.setOnClickListener(new OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				//Toast.makeText(getBaseContext(), "Button clicked!", Toast.LENGTH_LONG).show();
-				TextView tv = (TextView) findViewById(R.id.textView);
-				tv.setText("Button was clicked!");
-				((Button) v).setText("Click me again, please!");
-			}
-        	
-        });
+        /*ImageView image1 = (ImageView) this.findViewById(R.id.imageView2);
+        QuickContactBadge button1 = (QuickContactBadge) this.findViewById(R.id.player1);
+        QuickContactBadge button2 = (QuickContactBadge) this.findViewById(R.id.player2);
+        QuickContactBadge button3 = (QuickContactBadge) this.findViewById(R.id.player3);
+        QuickContactBadge button10 = (QuickContactBadge) this.findViewById(R.id.player10);
+        QuickContactBadge button11 = (QuickContactBadge) this.findViewById(R.id.player11);
+        QuickContactBadge button12 = (QuickContactBadge) this.findViewById(R.id.player12);
+        ImageView image = (ImageView) this.findViewById(R.id.imageView1);
+        QuickContactBadge button4 = (QuickContactBadge) this.findViewById(R.id.player4);
+        QuickContactBadge button5 = (QuickContactBadge) this.findViewById(R.id.player5);
+        QuickContactBadge button6 = (QuickContactBadge) this.findViewById(R.id.player6);
+        ImageView image3 = (ImageView) this.findViewById(R.id.imageView3);
+        QuickContactBadge button7 = (QuickContactBadge) this.findViewById(R.id.player7);
+        QuickContactBadge button8 = (QuickContactBadge) this.findViewById(R.id.player8);
+        QuickContactBadge button9 = (QuickContactBadge) this.findViewById(R.id.player9);
+        */
+      
     }
 }
