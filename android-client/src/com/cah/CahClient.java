062713ecfee8b92b5c245fea9cea2c06171c4c54
James Miller V <mille168@purdue.edu>
2013-02-25 15:43:41 -0500

    WIP

--- android-client/src/com/cah/CahClient.java
+++ android-client/src/com/cah/CahClient.java
@@ -40,7 +40,7 @@ class ActionDelta extends Delta {
 
 }
 
-public class CahClient implements Runnable{
+public class CahClient implements Runnable {
 
 	static Queue<Delta> incoming;
 	static Queue<Delta> outgoing;
@@ -70,28 +70,59 @@ public class CahClient implements Runnable{
 		outgoing = out;
 	}
 
-	public void run() {
+	Socket socket;
 
-		Socket socket = null;
+	public Encode() {
+		JsonWriter writer = new JsonWriter(	new OutputStreamWriter( socket.getOutputStream(), "UTF-8" ) );
+		while( true ) {
+			Delta message_out;
+			while( ( message_out = out.poll() ) != null ) {
+				gson.toJson(message_out, message_out.getClass(), writer);
+				writer.flush();
+			}
+			Thread.yield();
+		}
+	}
+
+	private class Decoder extends Thread {
+		public run() {
+			JsonReader reader = new JsonReader(	new BufferedReader(new InputStreamReader( socket.getInputStream())));
+
+			while( true ) {
+				Delta message_in;
+
+				reader.beginObject();
+
+				String name = reader.nextName();
+				if( name.equals("name") ) {
+					username = reader.nextString();
+				} else if( name.equals("followers_count") ) {
+					followersCount = reader.nextInt();
+				} else {
+					//error
+				}
+
+				JsonElement jelement = new JsonParser().parse(jsonLine);
+				JsonObject  jobject = jelement.getAsJsonObject();
+				jobject = jobject.getAsJsonObject("data");
+				JsonArray jarray = jobject.getAsJsonArray("translations");
+				jobject = jarray.get(0).getAsJsonObject();
+				String result = jobject.get("translatedText").toString();
+
+				reader.endObject();
+			}
+		}
+	}
+
+	public void run() {
 		try {
+			Decode d = new Decode();
 			socket = new Socket("localhost", 41337);
 			socket.setTcpNoDelay(true);
-			JsonWriter writer = new JsonWriter(	new OutputStreamWriter( socket.getOutputStream(), "UTF-8" ) );
-			//JsonReader reader = new JsonReader(	new BufferedReader(new InputStreamReader( socket.getInputStream())));
-			BufferedReader buffin = new BufferedReader(new InputStreamReader( socket.getInputStream()));
-
-			TableDelta td = new TableDelta();
-			td.Command = "new";
-
-			gson.toJson(td, TableDelta.class, writer);
-			writer.flush();
-
-			System.out.println("\n Written \n");
-
-			System.out.println("Reply");
-			System.out.println(buffin.readLine());
-
+			d.start();
+			Encode();
 		} catch (UnknownHostException e) {
+			System.out.println("Unknown Host: "+e);
 		} catch (IOException e) {
 			System.out.println("IO Exception: "+e);
 		}
