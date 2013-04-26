a5f056d6946e1212c6d5c2d655aa50cfcffc9b03
galarnyk <galarnyk@gmail.com>
2013-04-26 12:42:46 -0400

    Fixed scroll resize issue

--- android-client/src/com/cah/customviews/CardView.java
+++ android-client/src/com/cah/customviews/CardView.java
@@ -92,7 +92,7 @@ public class CardView extends View {
 
 	}
 	
-	private void invalidateSizeOfEverything() {
+	public void invalidateSizeOfEverything() {
 		// mCahLogo should be 60% of the card's width
 		int goalLogoWidth = (mCardWidth / 10) * 5;
 		int goalLogoHeight = (int) (goalLogoWidth/5.8);
@@ -113,8 +113,10 @@ public class CardView extends View {
 		
 		if(mCardWidth < 0) {
 			mCardWidth = getWidth();
-			invalidateSizeOfEverything();
+			//invalidateSizeOfEverything();
 		}
+		
+		invalidateSizeOfEverything();
 
 		// TODO: consider storing these as member variables to reduce
 		// allocations per draw cycle.
